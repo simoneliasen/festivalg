de7b86c67d2ca33c4fbecbb6ad825146b1b1245b
Mona Lisa <simoneliasen@gmail.comgit>
2019-07-02 20:11:13 +0200

    Writing artistnames to db

--- processes/artistnames.py
+++ processes/artistnames.py
@@ -1,4 +1,5 @@
 #Webscraper for Roskilde Festival
+import mysql.connector #Import Mysql library
 import requests #Import Requests Library
 result = requests.get("https://www.roskilde-festival.dk/en/line-up/") #Select page to scrape
 
@@ -19,6 +20,27 @@ for artist in samples:
     print(list(artist.stripped_strings)[0])
 
 # Check for lenght throw error if empty?? made by echo (i don't get this)
+for artist in samples:
 x = list(artist.stripped_strings)
 if x:
     print(x[0])
+
+#Insert list of elements into Sql database
+import mysql.connector
+
+# Database credentials
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="festivalg"
+)
+
+mycursor = mydb.cursor()
+
+# Add sql query and values
+sql = "INSERT INTO roskildeartists (name) VALUES (%s)"
+
+mycursor.executemany(sql, ??) #How to put entire list into db?
+
+mydb.commit()
