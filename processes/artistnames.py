32b211b2a049d10100f79b68ea23ecba48886f5d
Mona Lisa <simoneliasen@gmail.comgit>
2019-07-03 17:46:08 +0200

    Removed unecessary code

--- processes/artistnames.py
+++ processes/artistnames.py
@@ -3,10 +3,6 @@ import mysql.connector #Import Mysql library
 import requests #Import Requests Library
 result = requests.get("https://www.roskilde-festival.dk/en/line-up/") #Select page to scrape
 
-#print(result.status_code) #Make sure we got a result (should return 200)
-#print(result.headers) #Return header info
-#print(result.content) #Returrns all results from page
-
 # Store content in accesible variable
 c = result.content
 
@@ -19,9 +15,6 @@ samples = soup.find_all("a", "name")
 artist_names = []
 for artist in samples:
     artist_names.append(list(artist.stripped_strings)[0]) #adds stripped strings to list
-
-#Insert list of elements into Sql database
-import mysql.connector
 
 # Database credentials
 mydb = mysql.connector.connect(
@@ -38,8 +31,6 @@ sql = "INSERT INTO roskildeartists (name) VALUES (%s)"
 mycursor.executemany(sql, [[name] for name in artist_names]) #Puts list into db, with each element being own list
 
 mydb.commit()
-
-
 
 # Check for lenght throw error if empty?? made by echo (i don't get this)
 # x = list(artist.stripped_strings)
