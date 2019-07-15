dc94d24adf399df964df2316eb8e8ccacba46fca
Mona Lisa <simoneliasen@gmail.comgit>
2019-07-15 22:00:47 +0200

    Adjusted search route

--- festivalg.py
+++ festivalg.py
@@ -1,35 +1,35 @@
 #Import flask class + render_template class for acessing html templates Import url_for, for serving static files (css,js)
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import mysql.connector
+import json
+
 app = Flask(__name__)
 
 #Festival Class
-class festival:
-    def __init__(self, name, description, price, location, date, artists, weather, genre, youtube_showreel,):
-        self.name = name #Manual
-        self.description = description #Manual
-        self.price = price #Manual (add buy link?)
-        self.location = location #Manual
-        self.date = date #Manual
-        self.artists = artists #Nested artist objects
-        self.new_artists = new_artists #difference from old scrape to new scrape
-        self.weather = weather #Weather API
-        self.genre = genre #Manual
-        self.youtube_showreel = youtube_showreel #Manual
+# class festival:
+#     def __init__(self, name, description, price, location, date, artists, weather, genre, youtube_showreel,):
+#         self.name = name #Manual
+#         self.description = description #Manual
+#         self.price = price #Manual (add buy link?)
+#         self.location = location #Manual
+#         self.date = date #Manual
+#         self.artists = artists #Nested artist objects
+#         self.new_artists = new_artists #difference from old scrape to new scrape
+#         self.weather = weather #Weather API
+#         self.genre = genre #Manual
+#         self.youtube_showreel = youtube_showreel #Manual
 
 # artists class
-class artist:
-    def __init__(self, name, image, genre, description, country, festival, top_tracks,):
-        self.name = name #Webscrape festival site
-        self.image = image #SpotifyAPI
-        self.genre = genre #SpotifyAPI
-        self.description = description #SpotifyAPI (maybe not avaliable?)
-        self.country = country #SpotifyAPi
-        self.festival = festival #Parent Class
-        self.top_tracks = top_tracks #SpotifyApi
+# class artist:
+#     def __init__(self, name, image, genre, description, country, festival, top_tracks,):
+#         self.name = name #Webscrape festival site
+#         self.image = image #SpotifyAPI
+#         self.genre = genre #SpotifyAPI
+#         self.description = description #SpotifyAPI (maybe not avaliable?)
+#         self.country = country #SpotifyAPi
+#         self.festival = festival #Parent Class
+#         self.top_tracks = top_tracks #SpotifyApi
 
-# Connecting with database to deliver search options
-import mysql.connector
-import json
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -43,6 +43,7 @@ cur.execute("SELECT name FROM roskildefestival")
 result_list = [row[0] for row in cur.fetchall()]
 roskildeartists = (json.dumps(result_list))
 
+
 festivals = [
     {
         'name': 'Roskilde Festival',
@@ -53,50 +54,54 @@ festivals = [
         'artists': 'many',
         'genre': 'Everything',
         'date': 'April 20, 2018'
-    },
-    {
-        'name': 'Northside',
-        'price': '1800',
-        'description': 'Very good festival',
-        'location': 'Aarhus',
-        'weather': 'Sunny',
-        'artists': 'many',
-        'genre': 'Everything',
-        'date': 'April 25, 2018'
     }
 ]
 
 artists = [
     {
-        'name': 'Asap Rocky',
+        'name': 'Bob',
         'image': 'https://i.scdn.co/image/6d869bf0d56329a00bece664e8cd9f92e4fa83d7',
-        'country': 'Norway',
+        'country': 'Denmark',
         'genre': 'Hiphop',
         'toptracks': 'toptracks here',
         'festival': 'Roskilde'
     },
     {
-        'name': 'Tame Impala',
+        'name': 'Alice',
         'image': 'https://i.scdn.co/image/6d869bf0d56329a00bece664e8cd9f92e4fa83d7',
         'country': 'Denmark',
-        'genre': 'Pop',
+        'genre': 'Hiphop',
         'toptracks': 'toptracks here',
-        'festival': 'Northside'
+        'festival': 'Roskilde'
     }
 ]
 
+
+# Default route
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', festivals=festivals, roskildeartists=roskildeartists)
+    return render_template('home.html', roskildeartists=roskildeartists)
 
+#Search route artist
+@app.route("/artist", methods=["GET", "POST"])
+def artist():
+    if request.method == "GET":
+        return "Please submit the form instead."
+    else:
+        artists = request.form.get("search")
+        return render_template("artist.html", artists=artists)
+
+
+
+#Search route festival
 @app.route("/festival")
 def festival():
     return render_template('festival.html', title='Festival', festivals=festivals)
 
-@app.route("/artist")
-def artist():
-    return render_template('artist.html', title='Artists', artists=artists)
+# @app.route("/artist")
+# def artist():
+#     return render_template('artist.html', title='Artists', artists=artists)
 
 if __name__ == '__main__':
     app.run(debug=True)
