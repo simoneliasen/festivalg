0755b6e26b61fd7816598870ead62b2c39b68363
Mona Lisa <simoneliasen@gmail.comgit>
2019-07-28 16:12:29 +0200

    Register search input in variable, to later match mysql tables

--- festivalg.py
+++ festivalg.py
@@ -5,18 +5,7 @@ import json
 
 app = Flask(__name__)
 
-# artists class
-# class artist:
-#     def __init__(self, name, image, genre, description, country, festival, top_tracks,):
-#         self.name = name #Webscrape festival site
-#         self.image = image #SpotifyAPI
-#         self.genre = genre #SpotifyAPI
-#         self.description = description #SpotifyAPI (maybe not avaliable?)
-#         self.country = country #SpotifyAPi
-#         self.festival = festival #Parent Class
-#         self.top_tracks = top_tracks #SpotifyApi
-
-
+# Mysql connection
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -24,50 +13,26 @@ mydb = mysql.connector.connect(
   database="festivalg"
 )
 
+# display Mysql names for search autocomplete
 cur = mydb.cursor()
 cur.execute("SELECT name FROM roskildefestival")
 result_list = [row[0] for row in cur.fetchall()]
 roskildeartists = (json.dumps(result_list))
 
-
-artists = [
-    {
-        'name': 'Bob',
-        'image': 'https://i.scdn.co/image/6d869bf0d56329a00bece664e8cd9f92e4fa83d7',
-        'country': 'Denmark',
-        'genre': 'Hiphop',
-        'toptracks': 'toptracks here',
-        'festival': 'Roskilde'
-    },
-    {
-        'name': 'Alice',
-        'image': 'https://i.scdn.co/image/6d869bf0d56329a00bece664e8cd9f92e4fa83d7',
-        'country': 'Denmark',
-        'genre': 'Hiphop',
-        'toptracks': 'toptracks here',
-        'festival': 'Roskilde'
-    }
-]
-
-
-# Default route
+# Home page
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', roskildeartists=roskildeartists)
 
-#Search route artist
-@app.route("/artist", methods=["GET", "POST"])
-def artist():
+# Artist page
+@app.route('/artist', methods=["GET", "POST"])
+def artist(): #If Attempt to GET is made, display error
     if request.method == "GET":
-        return "Please submit the form instead."
-    else:
-        artists = request.form.get("search")
-        return render_template("artist.html", artists=artists)
-
-# @app.route("/artist")
-# def artist():
-#     return render_template('artist.html', title='Artists', artists=artists)
+              return "Please submit the form instead."
+    else: # Takes input from searchbar to variable artist
+        artist = request.form['search']
+        return render_template("artist.html", artist = artist)
 
 if __name__ == '__main__':
     app.run(debug=True)
