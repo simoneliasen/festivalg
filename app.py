7060a132fe9a7f75e7469ffde4644f99fb6a1ac3
LinuxBoii <simoneliasen@gmail.com>
2019-09-17 22:22:03 +0200

    wrote mysql out of application

--- app.py
+++ app.py
@@ -1,34 +1,45 @@
 from flask import Flask, render_template, url_for, request
 from config import DevelopmentConfig
-import mysql.connector
+#import mysql.connector
 import json
 import os
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
 app = Flask(__name__)
-
 app.config.from_object(DevelopmentConfig)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
 db = SQLAlchemy(app)
 
-from models import *
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(300), unique=True, nullable=False)
+    img = db.Column(db.String())
+    uri = db.Column(db.String())
+    festival = db.Column(db.String())
+#default='default.jpg' is an option
 
-# Mysql connection
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  passwd="",
-  database="festivalg"
-)
+    def __repr__(self):
+        return f"Artist('{self.name}', '{self.img}', '{self.uri}', '{self.festivalg}')"
+
+# from models import *
+
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   passwd="",
+#   database="festivalg"
+# )
 
 # display Mysql names for search autocomplete
-cur = mydb.cursor()
-cur.execute("SELECT name FROM roskildefestival")
-result_list = [row[0] for row in cur.fetchall()]
-roskildeartists = (json.dumps(result_list))
+#cur = mydb.cursor()
+#cur.execute("SELECT name FROM roskildefestival")
+#result_list = [row[0] for row in cur.fetchall()]
+
+List = ["Geeks", "For", "Geeks"] 
+roskildeartists = (json.dumps(List))
+
 
 # Home page
 @app.route("/")
@@ -42,13 +53,14 @@ def artist():
     if request.method == "GET":
               return "Please submit the form instead."
     else:
+        print('Close enough')
         #Input from form
-        search_input = request.form.get("search")
+ #       search_input = request.form.get("search")
         #Use data from database
-        cur = mydb.cursor()
-        cur.execute("SELECT * FROM roskildefestival WHERE name = %s", (search_input,))
-        artist_data = cur.fetchall()
-        return render_template("artist.html", search_input = search_input, artist_data = artist_data)
+  #      cur = mydb.cursor()
+   #     cur.execute("SELECT * FROM roskildefestival WHERE name = %s", (search_input,))
+    #    artist_data = cur.fetchall()
+     #   return render_template("artist.html", search_input = search_input, artist_data = artist_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,13 +1,13 @@
-from app import db
+# from app import db
 
-class Artist(db.Model):
+# class Artist(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    img = db.Column(db.String())
-    uri = db.Column(db.String())
-    festival = db.Column(db.String())
+#     # id = db.Column(db.Integer, primary_key=True)
+#     # name = db.Column(db.String())
+#     # img = db.Column(db.String())
+#     # uri = db.Column(db.String())
+#     # festival = db.Column(db.String())
 
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+#     def __repr__(self):
+#         return '<id {}>'.format(self.id)
     
