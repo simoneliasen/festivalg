796161b89a72f466298b933c828c921a9ae6c2de
simoneliasen <simoneliasen@gmail.com>
2019-09-20 18:24:13 +0200

    stable version, appending data to db works

--- config.py
+++ config.py
@@ -8,20 +8,16 @@ class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-
 class ProductionConfig(Config):
     DEBUG = False
-
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
-
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-
 
 class TestingConfig(Config):
     TESTING = True
